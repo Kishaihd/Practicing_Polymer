b7ff34058c270c93bdcaf409e5b3a749ce654067
Kaden Shaihd <mr.pinstripe_suit@hotmail.com>
2014-11-17 17:15:21 -0700

    Calculates skills + ability modifiers.
    FINALLY!
    Now working on a getter function that can return any skill. Maybe try
    int getSkill(String skillName) ?

--- lib/model/Entity.dart
+++ lib/model/Entity.dart
@@ -130,19 +130,20 @@ class Entity {
     abilitiesForSkills = [_strength, _dexterity, _intelligence, _wisdom, _charisma];
   }
   
-  int calculateSave(int abilityScore) {
+  int calcAbilityMod(int abilityScore) {
     return (abilityScore/2 - 5).floor();
   }
   
   // Run after race (and class?) is/are selected.
-  void skillsPlusAbilities() {
-    for (int i = 0; i < skillList.length; i++) {
-      for (int value in skillList) {
-        value += abilitiesForSkills[i];
-      }
+  void skillsPlusAbilities() {    
+//   List<Map> newSkillList = [];
+    for (int i = 0; i < abilitiesForSkills.length; i++) {     
+      skillList[i].forEach((String skillName,int skillRank) { 
+//        newSkillList[i].
+        skillList[i][skillName] = skillRank + calcAbilityMod(abilitiesForSkills[i]);
+      }); 
     }
   }
-
   // Runs a few smaller functions to calculate final skill score?
   void calcSkills(int abilityScore) {
     
@@ -166,161 +167,185 @@ class Entity {
   
   void changeStatus(String newStatus) {
     _status = newStatus;
+  // Throw in some if (_status == _____) 
+    // statements to add mechanics and maybe flavor text.
+    
   }
   
-  
-  
-}
-
-
-
-
-
-// Shit for practicing shit
-import 'dart:io';
-
-void main() {
-
-int _strength;
-int _dexterity;
-int _constitution;
-int _intelligence;
-int _wisdom;
-int _charisma; 
-
-print("Input strength");
-_strength = int.parse(stdin.readLineSync());
-
-print("Input dexterity");
-_dexterity = int.parse(stdin.readLineSync());
-
-print("Input con");
-_constitution = int.parse(stdin.readLineSync());
-
-print("Input intelligence");
-_intelligence = int.parse(stdin.readLineSync());
-
-print("Input wisdom");
-_wisdom = int.parse(stdin.readLineSync());
-
-print("Input charisma");
-_charisma = int.parse(stdin.readLineSync());
-
-Map<String, int> abilities = { // Write getters for all these and use them instead of the raw private int.
-"Strength": _strength,
-"Dexterity": _dexterity,
-"Constitution": _constitution, 
-"Intelligence": _intelligence,
-"Wisdom": _wisdom,
-"Charisma": _charisma
-};
-
-// Skill lists (as maps)
-Map<String, int> strSkills = {
-"athletics": 0
-};
-
-Map<String, int> dexSkills = {
-"acrobatics": 0,
-"sleight of hand": 0,     
-"stealth": 0
-};
-
-Map<String, int> intSkills = {
-"arcana": 0,
-"history": 0,
-"investigation": 0,
-"nature": 0,
-"religion": 0      
-};
-
-Map<String, int> wisSkills = {
-"animal handling": 0,
-"insight": 0,
-"medicine": 0,
-"perception": 0,
-"survival": 0
-};
-
-Map<String, int> chaSkills = {
-"deception": 0,
-"intimidation": 0,
-"performance": 0,
-"persuasion": 0
-};
-
-//  List<Map> skillList = [];
-
-// Does not include Constitution.
-List<int> abilitiesForSkills = [];
-
-List<Map> skillList = [strSkills, dexSkills, intSkills, wisSkills, chaSkills];
-abilitiesForSkills = [_strength, _dexterity, _intelligence, _wisdom, _charisma];
-
-
-int calcAbilityMod(int abilityScore) {
-return (abilityScore/2 - 5).floor();
-}
-
-//  Map newMap(Map oldMap) {
-//    int i = 0;
-//    Map<String, int> newMap;
-//    while (i < oldMap.length) {
-//      newMap
-//      i++;
-//    }
-//    
-//    return new Map.from(oldMap);
-//  }
-
-// Run after race (and class?) is/are selected.
-void skillsPlusAbilities() {    
- List<Map> newSkillList = [];
- List<String> keys;
+  int get strength => _strength;
+  int get dexterity => _dexterity;
+  int get constitution => _constitution;
+  int get intelligence => _intelligence;
+  int get wisdom => _wisdom;
+  int get charisma => _charisma;
  
- for (int i = 0; i < abilitiesForSkills.length; i++) {
-  for (int j = 0; j < skillList.elementAt(i).length; j++) {
-//       keys[j] = skillList[i][j];
-//       String key = keys[j];
-   for (Map currentSkillMap in skillList.elementAt(j))  
-     currentSkillMap[String] += calcAbilityMod(abilitiesForSkills[i]);
-//      skillList.forEach((Map<String, int> map) {
-//        currentSkillMap.forEach((String k, int v){v += calcAbilityMod(abilitiesForSkills[i]);
-//      });
-//    });
-}
- }
-//     //skillList[i].forEach((String k, int value) {
-//     skillList[i][key] += calcAbilityMod(abilitiesForSkills[i]);
+  int get (String skillName) {
+    int thisSkill; 
     
-//     newSkillList[i][k][value] = skillList[i][k][value + calcAbilityMod(abilitiesForSkills[i])];        
-//        int score = value;
-//        score += calcAbilityMod(abilitiesForSkills[i]);
-//        value = score;
-//     });
-//  }
+    for (int i = 0; i < skillList.length; i++) {     
+          skillList[i]. ((String skillName,int skillRank) { 
+//        newSkillList[i].
+            skillList[i][skillName] = skillRank + calcAbilityMod(abilitiesForSkills[i]);
+          });
+    
+    
+    for (int i = 0; i < skillList.length; i++) {
+      skillList[i][skillList.singleWhere( == skillName)];
+    }
+    return skillList[skillList.singleWhere( == skillName)];
+  }
+  
 }
-//  skillList = newSkillList;
-//  newSkillList.clear();
+
+
+
+
+
+//// Shit for practicing shit
+//import 'dart:io';
+//
+//void main() {
+//
+//int _strength;
+//int _dexterity;
+//int _constitution;
+//int _intelligence;
+//int _wisdom;
+//int _charisma; 
+//
+//print("Input strength");
+//_strength = int.parse(stdin.readLineSync());
+//
+//print("Input dexterity");
+//_dexterity = int.parse(stdin.readLineSync());
+//
+//print("Input con");
+//_constitution = int.parse(stdin.readLineSync());
+//
+//print("Input intelligence");
+//_intelligence = int.parse(stdin.readLineSync());
+//
+//print("Input wisdom");
+//_wisdom = int.parse(stdin.readLineSync());
+//
+//print("Input charisma");
+//_charisma = int.parse(stdin.readLineSync());
+//
+//Map<String, int> abilities = { // Write getters for all these and use them instead of the raw private int.
+//"Strength": _strength,
+//"Dexterity": _dexterity,
+//"Constitution": _constitution, 
+//"Intelligence": _intelligence,
+//"Wisdom": _wisdom,
+//"Charisma": _charisma
+//};
+//
+//// Skill lists (as maps)
+//Map<String, int> strSkills = {
+//"athletics": 0
+//};
+//
+//Map<String, int> dexSkills = {
+//"acrobatics": 0,
+//"sleight of hand": 0,     
+//"stealth": 0
+//};
+//
+//Map<String, int> intSkills = {
+//"arcana": 0,
+//"history": 0,
+//"investigation": 0,
+//"nature": 0,
+//"religion": 0      
+//};
+//
+//Map<String, int> wisSkills = {
+//"animal handling": 0,
+//"insight": 0,
+//"medicine": 0,
+//"perception": 0,
+//"survival": 0
+//};
+//
+//Map<String, int> chaSkills = {
+//"deception": 0,
+//"intimidation": 0,
+//"performance": 0,
+//"persuasion": 0
+//};
+//
+////  List<Map> skillList = [];
+//
+//// Does not include Constitution.
+//List<int> abilitiesForSkills = [];
+//
+//List<Map> skillList = [strSkills, dexSkills, intSkills, wisSkills, chaSkills];
+//abilitiesForSkills = [_strength, _dexterity, _intelligence, _wisdom, _charisma];
+//
+//
+//int calcAbilityMod(int abilityScore) {
+//return (abilityScore/2 - 5).floor();
+//}
+//
+////  Map newMap(Map oldMap) {
+////    int i = 0;
+////    Map<String, int> newMap;
+////    while (i < oldMap.length) {
+////      newMap
+////      i++;
+////    }
+////    
+////    return new Map.from(oldMap);
+////  }
+//
+//// Run after race (and class?) is/are selected.
+//void skillsPlusAbilities() {    
+// List<Map> newSkillList = [];
+// List<String> keys;
+// 
+// for (int i = 0; i < abilitiesForSkills.length; i++) {
+//  for (int j = 0; j < skillList.elementAt(i).length; j++) {
+////       keys[j] = skillList[i][j];
+////       String key = keys[j];
+//   for (Map currentSkillMap in skillList.elementAt(j))  
+//     currentSkillMap[String] += calcAbilityMod(abilitiesForSkills[i]);
+////      skillList.forEach((Map<String, int> map) {
+////        currentSkillMap.forEach((String k, int v){v += calcAbilityMod(abilitiesForSkills[i]);
+////      });
+////    });
+//}
 // }
-
-String printSkills() {
-print(skillList.toString());
-return skillList.toString();
-}
-
-String printAbilitiesAndMods() {
-abilities.forEach((String k, int v) => print("$k : $v Modifier: ${calcAbilityMod(v)} \n"));
-String msg = "${abilities.forEach((String k, int v) => "$k : $v \n")}";
-return msg;
-}
-
-
-skillsPlusAbilities();
-
-printAbilitiesAndMods();
-printSkills();  
-
-
-}
-
+////     //skillList[i].forEach((String k, int value) {
+////     skillList[i][key] += calcAbilityMod(abilitiesForSkills[i]);
+//    
+////     newSkillList[i][k][value] = skillList[i][k][value + calcAbilityMod(abilitiesForSkills[i])];        
+////        int score = value;
+////        score += calcAbilityMod(abilitiesForSkills[i]);
+////        value = score;
+////     });
+////  }
+//}
+////  skillList = newSkillList;
+////  newSkillList.clear();
+//// }
+//
+//String printSkills() {
+//print(skillList.toString());
+//return skillList.toString();
+//}
+//
+//String printAbilitiesAndMods() {
+//abilities.forEach((String k, int v) => print("$k : $v Modifier: ${calcAbilityMod(v)} \n"));
+//String msg = "${abilities.forEach((String k, int v) => "$k : $v \n")}";
+//return msg;
+//}
+//
+//
+//skillsPlusAbilities();
+//
+//printAbilitiesAndMods();
+//printSkills();  
+//
+//
+//}
+//
