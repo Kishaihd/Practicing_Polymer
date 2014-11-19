e720ec3b74b595f9b71d85fb0a3be51ef3cb502a
Kaden Shaihd <mr.pinstripe_suit@hotmail.com>
2014-11-18 20:38:49 -0700

    Class-ifying things.
    Ability Class. Soon to come: Skill class?

--- lib/model/Entity.dart
+++ lib/model/Entity.dart
@@ -1,11 +1,13 @@
 library model.Entity;
 
+import 'dart:io';
+
 class Entity {
   String _name;
   String _race;
   String _type; // eg. Humanoid, Abberation, Construct etc.
   String _alignment;
-  int _size;
+  String _size;
   int _level;
   int _hitDie;
   int _maxHitPoints;
@@ -63,71 +65,20 @@ class Entity {
   // Does not include Constitution.
   List<int> abilitiesForSkills = [];
   
+//  Entity();
   
-//  List<Map> skillsList = [ // Str 0, Dex 1, Int 2, Wis 3, Cha 4
-//    { // Strength
-//      "athletics": 0
-//      },
-//    { // Dexterity
-//      "acrobatics": 0,
-//      "sleight of hand": 0,     
-//      "stealth": 0
-//      },
-//    { // Intelligence
-//      "arcana": 0,
-//      "history": 0,
-//      "investigation": 0,
-//      "nature": 0,
-//      "religion": 0
-//      },
-//    { // Wisdom
-//        "animal handling": 0,
-//        "insight": 0,
-//        "medicine": 0,
-//        "perception": 0,
-//        "survival": 0        
-//      },
-//    { // Charisma
-//        "deception": 0,
-//        "intimidation": 0,
-//        "performance": 0,
-//        "persuasion": 0       
-//      }
-//      
-//  ];
-  
-  
-//  Map<String, int> skills = {
-//     // Strength
-//     "athletics": 0,
-//     // Dexterity
-//     "acrobatics": 0,
-//     "sleight of hand": 0,     
-//     "stealth": 0,
-//     // Intelligence
-//     "arcana": 0,
-//     "history": 0,
-//     "investigation": 0,
-//     "nature": 0,
-//     "religion": 0,
-//     // Wisdom
-//     "animal handling": 0,
-//     "insight": 0,
-//     "medicine": 0,
-//     "perception": 0,
-//     "survival": 0,
-//     // Charisma
-//     "deception": 0,
-//     "intimidation": 0,
-//     "performance": 0,
-//     "persuasion": 0
-//  };
-  
+  // Parameterized constructor.
+  // Name, maxHP, ability scores.
   Entity(this._name, this._maxHitPoints, this._strength, this._dexterity, this._constitution, this._intelligence, this._wisdom, this._charisma) {
     _currentHitPoints = _maxHitPoints;
     _status = "normal";
-    skillList = [strSkills, dexSkills, intSkills, wisSkills, chaSkills];
+    final skillList = [strSkills, dexSkills, intSkills, wisSkills, chaSkills];
+    refactor();
+  }
+  
+  void refactor() {
     abilitiesForSkills = [_strength, _dexterity, _intelligence, _wisdom, _charisma];
+        
   }
   
   int calcAbilityMod(int abilityScore) {
@@ -136,17 +87,75 @@ class Entity {
   
   // Run after race (and class?) is/are selected.
   void skillsPlusAbilities() {    
-//   List<Map> newSkillList = [];
+    List<Map> newSkillList = [];
     for (int i = 0; i < abilitiesForSkills.length; i++) {     
       skillList[i].forEach((String skillName,int skillRank) { 
-//        newSkillList[i].
         skillList[i][skillName] = skillRank + calcAbilityMod(abilitiesForSkills[i]);
-      }); 
+            }); 
+    }
+  } 
+
+  // Adds racial bonuses to abilities and skills?
+  void addRace() {
+    switch (_race) {
+      case 'human':
+        allAbilities.forEach((String ability, int value) {
+          allAbilities[ability] = value + 1;
+      });
+        
+        break;
+      case 'elf':
+        
+        break;
+      default:
+        
+    }
+    
+    
+  }
+  
+  void chooseStatIncrease() {
+    print("This function is only for the command line version of this app!\n"
+          "Choose a stat to increase, nigga! (s)tr, (d)ex, (c)on, (i)nt, (w)is, (c)ha.");
+    String answer = stdin.readLineSync();
+    switch(answer) {
+      case 's':
+          
+      case 'd':
+        
+      case 'c':
+        
+      case 'i':
+        
+      case 'w':
+        
+      case 'c':
+        
+        break;
     }
   }
-  // Runs a few smaller functions to calculate final skill score?
-  void calcSkills(int abilityScore) {
-    
+  
+  void increaseStat(String stat, int bonus) {
+    stat = stat.toLowerCase();
+        int thisSkill; 
+        bool foundSkill;
+        int skillIdx = 0;
+        do {
+          if (skillIdx >= skillList.length) {
+            break;
+          }
+          foundSkill = (skillList[skillIdx].containsKey(stat));
+          thisSkill = skillList[skillIdx][stat];
+          skillIdx++;
+        } while (foundSkill == false);
+      
+        if (foundSkill == false) {
+          print("Skill not found!");
+        }
+        else {
+          skillIdx--;
+          skillList[skillIdx][stat] += bonus;
+        }
   }
   
   void takeDamage(int dmg) {
@@ -171,35 +180,129 @@ class Entity {
     // statements to add mechanics and maybe flavor text.
     
   }
-  // Save this stuff?
   
+  // Getters
   int get strength => _strength;
   int get dexterity => _dexterity;
   int get constitution => _constitution;
   int get intelligence => _intelligence;
   int get wisdom => _wisdom;
   int get charisma => _charisma;
- 
+  int get currentHP => _currentHitPoints;
+  int get level => _level;
+  int get HD => _hitDie;
+  int get maxHP => _maxHitPoints;
+  int get AC => _armorClass;
+  int get proficiencyBonus => _proficiency;
+  int get movement => _movement;
+  String get name => _name;
+  String get size => _size;
+  String get race => _race;
+  String get type => _type == null? "humanoid" : _type; // eg. Humanoid, Abberation, Construct etc.
+  String get allignment => _alignment;
+  String get status => _status;
+      
+  // "Generic" getter that returns any single skill and value.
   int getSkill(String skillName) {
+    skillName = skillName.toLowerCase();
     int thisSkill; 
-    
-    k
-    
-    for (int i = 0; i < skillList.length; i++) {     
-          skillList[i]. ((String skillName,int skillRank) { 
-            skillList[i][skillName] = skillRank + calcAbilityMod(abilitiesForSkills[i]);
-          });
-    
-    
-    for (int i = 0; i < skillList.length; i++) {
-      skillList[i][skillList.singleWhere( == skillName)];
+    bool foundSkill;
+    int skillIdx = 0;
+    do {
+      if (skillIdx >= skillList.length) {
+        break;
+      }
+      foundSkill = (skillList[skillIdx].containsKey(skillName));
+      thisSkill = skillList[skillIdx][skillName];
+      skillIdx++;
+    } while (foundSkill == false);
+  
+    if (foundSkill == false) {
+      print("Skill not found!");
+      return 0;
     }
-    return skillList[skillList.singleWhere( == skillName)];
+    else {
+      print("$skillName: $thisSkill");        
+      return thisSkill;
+    }      
+  }
+  
+  // Setters
+  void set strength(int str) { _strength = str;}
+  void set dexterity(int dex) {_dexterity = dex;}
+  void set constitution(int con) { _constitution = con;}
+  void set intelligence(int intl) { _intelligence = intl;}
+  void set wisdom(int wis) { _wisdom = wis;}
+  void set charisma(int cha) { _charisma = cha;}
+  void set currentHP(int hp) { _currentHitPoints = hp;}
+  void set size(String size) { _size = size;}
+  void set level(int lvl) { _level = lvl;}
+  void set HD(int hd) { _hitDie = hd;}
+  void set maxHP(int hpMax) {_maxHitPoints = hpMax;}
+  //void set AC(int ac) { _armorClass = ac;} // Calculated automatically.
+  //void set proficiencyBonus(int prof) { _proficiency = prof;}
+  //void set movement => _movement
+  void set name(String name) { _name = name;}
+  void set race(String race) { _race = race;}
+  void set type(String type) { _type = type;} // eg. Humanoid, Abberation, Construct etc.
+  void set allignment(String allignment) { _alignment = allignment;}
+  void set status(String status) { _status = status;}
+
+} // End class Entity  
+
+
+class Ability {
+  final int LIMIT = 20;
+  final String _name;
+  int _score;
+  int _mod;
+  
+  Ability(this._name, [this._score = 0]) {
+    calcMod();
+  }
+  
+  int get ability => _score;
+  int get mod => _mod;
+
+  @override String toString() => "${_name}: ${_score} (${modAsString()})";
+  String modAsString() => "${(_mod >= 0) ? '+' : ''}${_mod.toString()}"; 
+  
+  void calcMod() {
+    _mod = (_score/2 - 5).floor();
+  }
+  
+  // Cap set to 20, can change later.
+  void setAbility(int value) {
+    if (value > LIMIT) {
+      // Oh fuck off.
+      _score = LIMIT;
+    }
+    else if (value < 0) {
+      // Wtf are you doing.
+      _score = 0;      
+    }
+    else {
+      _score = value;
+    }   
+    calcMod();
+  }
+  
+  void increaseAbility(int value) {
+    _score += value;
+    calcMod();
+  }
+  void decreaseAbility(int value) {
+    _score -= value;
+    calcMod();
   }
   
 }
 
+// Will eventually split Entity into child classes, 
+//  but for now, Entity will be the entire Character class.
+//class Character extends Entity {
+//  Character();
+//}
 
-// Save to here wtf
 
 
